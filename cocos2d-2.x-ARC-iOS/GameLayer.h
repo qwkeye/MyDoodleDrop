8d81adda0b1821e054c1d2e8058cf276bfb8bb73
yzw <yzw668@qq.com>
2014-03-07 20:02:23 +0800

    add files

---
+++ cocos2d-2.x-ARC-iOS/GameLayer.h
@@ -0,0 +1,16 @@
+//
+//  GameLayer.h
+//  MyDoodleDrop
+//
+//  Created by yzw on 3/7/14.
+//  Copyright 2014 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface GameLayer : CCLayer {
+    
+}
++(id)scene;
+@end
---
+++ cocos2d-2.x-ARC-iOS/GameLayer.m
@@ -0,0 +1,17 @@
+//
+//  GameLayer.m
+//  MyDoodleDrop
+//
+//  Created by yzw on 3/7/14.
+//  Copyright 2014 __MyCompanyName__. All rights reserved.
+//
+
+#import "GameLayer.h"
+
+
+@implementation GameLayer
++(id)scene
+{
+    
+}
+@end
